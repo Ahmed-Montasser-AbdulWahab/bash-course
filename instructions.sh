82256a2c5f6e5c6bae2ba209a0cfff247c87c36b
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2025-01-05 15:59:04 +0200

    Arithmetic Operations

--- instructions.sh
+++ instructions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-read name
-test $name="Ahmed" && printf "True\n" || printf "False\n"
+read -sp "Enter Your Age" age
+echo
+read -sp "Enter the Current Year" current_year
+echo
+# let birth_year=$current_year-$age
+# expr $current_year - $age
+echo "You were born in $(($current_year - $age))"
 
-if [ $name="Ahmed" ]
-then
-printf "True\n"
-else
-printf "False\n"
-fi
-
-test 001 -eq 1 && echo "True" || echo "False"
-
-test -z $name && echo "Empty" || echo "Not Empty"
+x=0
+echo "$((++x))" #Substitution
+((x = $x * 4)) #No Substitution - ((C Expression))
+((x = x * 4)) #No Substitution - ((C Expression))
+echo $((x))
