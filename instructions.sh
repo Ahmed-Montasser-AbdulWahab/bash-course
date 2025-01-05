d328e2d726201f1cc34f98d3daabd68ae49e0b8f
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2025-01-05 13:50:30 +0200

    Demo Guess Game
    Using read , echo , if , test , shebang

--- instructions.sh
+++ instructions.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
-# set -x #for debug
-# for i in 1 2 3 4 5
-# do
-# echo ${i}
-# done
-# set +x
-
-# for i in {1..15}
-# do
-# echo $i
-# done
-
-# statement="I am Ahmed Montasser"
-# for i in $statement
-# do
-# echo $i
-# done
-# echo -n "Thank You"
-
-
-# for i in `ls`
-# do
-# echo $i
-# done
-
-# for s in `python3 demo.py`
-# do
-# echo $s
-# done
+target=20
+isFound=0
+for i in {1..5} 
+do
+read -sp "Enter a number between 0 and 40, This is trial : {${i}}" prediction
+if [ $prediction -eq $target ]
+then
+isFound=1
+break
+else
+printf "\nTry Again\n"
+fi
+done
+test $isFound -eq 1 && echo "You are winner" || echo "You are Loser"
