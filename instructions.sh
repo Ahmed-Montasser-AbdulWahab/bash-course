228c542230521407e4592165a0188cd9fae83f63
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2025-01-05 15:11:14 +0200

    IF and Test

--- instructions.sh
+++ instructions.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-target=20
-isFound=0
-for i in {1..5} 
-do
-read -sp "Enter a number between 0 and 40, This is trial : {${i}}" prediction
-if [ $prediction -eq $target ]
+read name
+test $name="Ahmed" && printf "True\n" || printf "False\n"
+
+if [ $name="Ahmed" ]
 then
-isFound=1
-break
+printf "True\n"
 else
-printf "\nTry Again\n"
+printf "False\n"
 fi
-done
-test $isFound -eq 1 && echo "You are winner" || echo "You are Loser"
+
+test 001 -eq 1 && echo "True" || echo "False"
+
+test -z $name && echo "Empty" || echo "Not Empty"
