abc3b7c6040fa540bf4ac6e25f7fbb651ed13e7e
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2025-01-05 12:36:46 +0200

    For Loop

--- instructions.sh
+++ instructions.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-read -p "Enter Your Name : " name
-# read -sp "Prompt" nameVariable
-echo -n "${name} is our favorite"
-#printf "${name} is our favorite\n"
+# set -x #for debug
+# for i in 1 2 3 4 5
+# do
+# echo ${i}
+# done
+# set +x
+
+# for i in {1..15}
+# do
+# echo $i
+# done
+
+# statement="I am Ahmed Montasser"
+# for i in $statement
+# do
+# echo $i
+# done
+# echo -n "Thank You"
+
+
+# for i in `ls`
+# do
+# echo $i
+# done
+
+# for s in `python3 demo.py`
+# do
+# echo $s
+# done
