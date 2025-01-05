32f1a4e1facadfecf69ead14f3e7ee91ac922421
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2025-01-05 16:44:16 +0200

    IF - Commenting

--- instructions.sh
+++ instructions.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-read -sp "Enter Your Age" age
-echo
-read -sp "Enter the Current Year" current_year
-echo
-# let birth_year=$current_year-$age
-# expr $current_year - $age
-echo "You were born in $(($current_year - $age))"
 
-x=0
-echo "$((++x))" #Substitution
-((x = $x * 4)) #No Substitution - ((C Expression))
-((x = x * 4)) #No Substitution - ((C Expression))
-echo $((x))
+#MultiLine Comment
+<<comment
+ if [ 1 -eq 1 -a 1 = 1 ] 
+ if [ 1 -eq 1 ] && [ 1 = 1 ]
+comment
+
+if [[ 1 -eq 1 && 1 = 1 ]]
+# if ((1 == 1))
+then
+echo "Both Conditions applied"
+else
+echo "None/Either of Conditions applied"
+fi
