22fa60a0c1dd0478b7dcc154360ce75223a2709c
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2025-01-06 14:35:00 +0200

    Switch Case

--- instructions.sh
+++ instructions.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
-set -x
-#Print Path and name of script running
-echo $0
-#$1-$9 print arguments
-echo $1
-# Print Count of arguments
-echo $#
-# Print Array of Arguments
-echo $@
-# Print String joining Arguments
-echo $*
-# Print Status Code of last Command
-echo $?
-# Print PID of current running script
-echo $$
-# # For loop
-# size=$#
-# args=("$@")
-# for((i=0 ; i<$size; i++))
-# do
-# echo $i
-# echo ${args[i]}
-# done
-# set +x
-
-for i in "$@"
-do
-echo $i
-done
+read -sp "Print Date" response
+echo
+case $response in
+yes|YES|Yes|Y|y)
+echo `date`
+;;
+*)
+echo 'NO'
+;;
+esac
